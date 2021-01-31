1f13ee1399e6f44f02cc8ab414b362eff7830cd8
samet346 <76067408+samet346@users.noreply.github.com>
2021-01-31 19:27:15 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstrcat;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // İş kodları
+            // yetkisi varmı 
+
+            return _productDal.GetAll();
+        }
+
+        
+    }
+}
