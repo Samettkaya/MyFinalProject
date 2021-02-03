5b9ff1af506d4b84766d0531d20d99b8e96e4850
samet346 <76067408+samet346@users.noreply.github.com>
2021-02-03 23:59:09 +0300

    Entity framework

---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstrcat;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        void IEntityRepository<Category>.Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEntityRepository<Category>.Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IEntityRepository<Category>.Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Category> IEntityRepository<Category>.GetAll(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEntityRepository<Category>.Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
